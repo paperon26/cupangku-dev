6af7ae5ab5f29bbb63dcd27bf6081600d9676499
paperon26 <71565125+paperon26@users.noreply.github.com>
2020-09-21 01:41:39 +0800

    create homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import HomePage from './pages/home.page/Home.page';
 
 function App() {
   return (
-    <div>
-      <h1>Paperon</h1>
+    <div className="App">
+      <HomePage />
     </div>
   );
 }
---
+++ src/pages/home.page/Home.page.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const HomePage = () => (
+	<div className="homepage">
+		<div className="directory-menu">
+			<div className="menu-item">
+				<div className="content">
+					<h1 className="title">Ikan</h1>
+					<span className="subtitle">List Ikan</span>
+				</div>
+			</div>
+
+			<div className="menu-item">
+				<div className="content">
+					<h1 className="title">Toko</h1>
+					<span className="subtitle">List Toko</span>
+				</div>
+			</div>
+
+			<div className="menu-item">
+				<div className="content">
+					<h1 className="title">Stok</h1>
+					<span className="subtitle">List Stok</span>
+				</div>
+			</div>
+
+		</div>
+	</div>
+)
+
+export default HomePage
